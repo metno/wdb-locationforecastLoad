b21a8903c83f8996f3e561ab17e0ee94d83d41bf
Vegard Bønes <vegard.bones@met.no>
2012-01-31 16:06:05 +0100

    autoconf and automake building

---
+++ wdb-locationforecastLoad/test/make.mk
@@ -0,0 +1,20 @@
+if HAVE_GTEST
+
+TESTS = locationforecastLoadTest
+
+check_PROGRAMS = locationforecastLoadTest
+
+locationforecastLoadTest_SOURCES = \
+	test/DataElementTest.cpp \
+	test/DocumentTest.cpp \
+	test/WdbSaveSpecificationFactoryTest.cpp
+
+locationforecastLoadTest_CPPFLAGS = \
+	$(AM_CPPFLAGS) \
+	$(gtest_CFLAGS)
+
+locationforecastLoadTest_LDADD = \
+	$(locationforecastLoad_LDADD) \
+	$(gtest_LIBS) -lgtest_main
+
+endif
